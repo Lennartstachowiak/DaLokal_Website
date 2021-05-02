000cc391c655607e049a5ae1d91bcf160276868f
LennyS <Lennart.Stachowiak@code.berlin>
2021-05-02 16:15:37 +0200

    test

--- dalokal_website/website.py
+++ dalokal_website/website.py
@@ -10,6 +10,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.secret_key = "hello"
 
+createDatabase()
+
 """ That connection is for GCloud """
 # Connection to database set up+
 def connectDatabase():
@@ -38,7 +40,109 @@ def connectDatabase():
 #     ).cursor()
 #     return cursor
 
-
+def createDatabase():
+    db_user = os.environ.get('CLOUD_SQL_USERNAME')
+    db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+    db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    cursor = pymysql.connect(
+        user=db_user,
+        password=db_password,
+        unix_socket=unix_socket,
+    )
+    cursor.execute('''
+        CREATE SCHEMA IF NOT EXISTS dalokalschema;
+        ''')
+        cursor.execute('USE dalokalschema;')
+        cursor.execute('SET autocommit=1')
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS user_table (
+                user_id INT(11) NOT NULL AUTO_INCREMENT,
+                email VARCHAR(255) NOT NULL,
+                password VARCHAR(500) NOT NULL,
+                firstname VARCHAR(45),
+                lastname VARCHAR(45),
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (user_id),
+            UNIQUE KEY unique_email (email),
+            UNIQUE KEY unique_user_id (user_id)
+            );''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS farm_table (
+            farm_id INT(11) NOT NULL AUTO_INCREMENT,
+            user_id INT(11) NOT NULL,
+            farmname VARCHAR(100) NOT NULL,
+            farm_img VARCHAR(100) NOT NULL,
+            description VARCHAR(1000) NOT NULL,
+            total_products INT(11) NOT NULL DEFAULT 0,
+            created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (farm_id),
+            UNIQUE KEY unique_farm_id (farm_id),
+            UNIQUE KEY unique_farmname (farmname),
+            CONSTRAINT farmToTable FOREIGN KEY (user_id)
+            REFERENCES user_table(user_id)
+        );''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS product_table (
+            product_id INT(11) NOT NULL AUTO_INCREMENT,
+            farm_id INT(11) NOT NULL,
+            category VARCHAR(45) NOT NULL,
+            product_name VARCHAR(45) NOT NULL,
+            product_img VARCHAR(100) NOT NULL,
+            product_description VARCHAR(500) NOT NULL,
+            product_price DECIMAL (10,2) NOT NULL,
+            product_weight DECIMAL (10,3) NOT NULL,
+            created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (product_id),
+            UNIQUE KEY unique_product_id (product_id),
+            CONSTRAINT productToFarm FOREIGN KEY (farm_id)
+            REFERENCES farm_table(farm_id)
+        );''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS time_table (
+            farm_id INT(11) NOT NULL,
+            day VARCHAR(45),
+            opening TIME,
+            closing TIME,
+            CONSTRAINT timeToFarm FOREIGN KEY (farm_id)
+            REFERENCES farm_table(farm_id)
+        );''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS category_table (
+            farm_id INT(11) NOT NULL,
+            veg_check TINYINT(4) NOT NULL DEFAULT '0',
+            milk_check TINYINT(4) NOT NULL DEFAULT '0',
+            wheat_check TINYINT(4) NOT NULL DEFAULT '0',
+            meat_check TINYINT(4) NOT NULL DEFAULT '0',
+            CONSTRAINT categoryToFarm FOREIGN KEY (farm_id)
+            REFERENCES farm_table(farm_id)
+        );''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS adress_table (
+            farm_id INT(11) NOT NULL,
+            street VARCHAR(100) NOT NULL,
+            postalcode INT(11) NOT NULL,
+            city VARCHAR(45) NOT NULL,
+            CONSTRAINT adressToFarm FOREIGN KEY (farm_id)
+            REFERENCES farm_table(farm_id)
+        );''')
+        cursor.execute('DROP TRIGGER IF EXISTS farm_total_products_increase;')
+        cursor.execute('DROP TRIGGER IF EXISTS farm_total_products_decrease;')
+        cursor.execute('''
+        CREATE TRIGGER farm_total_products_increase
+        AFTER INSERT ON product_table
+        FOR EACH ROW
+            UPDATE farm_table
+                SET total_products = total_products + 1
+            WHERE farm_id = NEW.farm_id;''')
+        cursor.execute('''
+        CREATE TRIGGER farm_total_products_decrease
+        AFTER DELETE ON product_table
+        FOR EACH ROW
+            UPDATE farm_table
+                SET total_products = total_products - 1
+            WHERE farm_id = OLD.farm_id;''')
+        cursor.close()
 
 def checkCompleteUser(userId):
     cursor = connectDatabase()
